0a4df1379ed6a296c4a618dd9fcda098ba6b3936
Thomas KOEBEL <thomas.koebel@epitech.eu>
2023-05-25 00:09:42 +0200

    Feat: graph integration, not enough performant

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'DeviceIcon.dart';
 import 'Dashboard.dart';
 import 'DashboardItem.dart';
 import 'Device.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,6 +41,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Device? device;
   Stream<String>? dataStream;
+  List<Data> data = [];
+  int initialTime = DateTime.now().millisecondsSinceEpoch;
 
   setDevice(Device? device) {
     setState(() {
@@ -73,15 +78,36 @@ class _MyHomePageState extends State<MyHomePage> {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Text('Awaiting result...');
                   }
-                  var data = snapshot.data ?? "0";
+                  if (snapshot.data != null) {
+                    this.data.add(Data(double.parse(snapshot.data!), initialTime));
+                  }
+                  if (this.data.length > 400) {
+                    this.data.removeAt(0);
+                  }
 
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text(
-                        data,
-                        style: Theme.of(context).textTheme.headline1,
+                      SfCartesianChart(
+                        primaryXAxis: CategoryAxis(
+                          title: AxisTitle(text: 'Time (s)'),
+                        ),
+                        tooltipBehavior: TooltipBehavior(enable: true),
+                        series: <ChartSeries<Data, String>>[
+                          LineSeries<Data, String>(
+                              dataSource: this.data,
+                              xValueMapper: (Data data, _) =>
+                                  data.time.toString(),
+                              yValueMapper: (Data data, _) => data.value,
+                              // Enable data label
+                              dataLabelSettings:
+                                  DataLabelSettings(isVisible: true))
+                        ],
                       ),
+                      // Text(
+                      //   data,
+                      //   style: Theme.of(context).textTheme.headline1,
+                      // ),
                     ],
                   );
                 },
@@ -90,4 +116,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ]),
         ));
   }
+}
+
+class Data {
+  Data(this.value, int inital) {
+    this.time = DateTime.now().millisecondsSinceEpoch - inital;
+  }
+  final double value;
+  late num time;
 }
